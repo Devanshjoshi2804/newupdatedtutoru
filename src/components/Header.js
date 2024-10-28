2f32837cab6caffd462876695df3d819b82bfa98
Devanshjoshi2804 <2022.devansh.joshi@ves.ac.in>
2024-10-28 11:07:33 +0530

    hello

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,9 +48,7 @@ function Header() {
           <Button component={Link} to="/find-tutor" variant="contained" color="primary">
             Find a Tutor
           </Button>
-          <Button component={Link} to="/contact" variant="outlined" color="primary">
-            Contact Us
-          </Button>
+          
         </div>
       </div>
       <nav ref={menuRef} className={`main-nav ${isMenuOpen ? 'open' : ''}`}>
@@ -59,7 +57,7 @@ function Header() {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/services">Services</Link></li>
           <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+          
         </ul>
       </nav>
     </header>
